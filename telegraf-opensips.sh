779a17ef8ea6c9fbbc2f01a65facf89a19310e5c
Akhil Jalagam <akhiljalagams@gmail.com>
2019-06-28 13:37:36 +0530

    Update telegraf-opensips.sh

--- telegraf-opensips.sh
+++ telegraf-opensips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Author: Akhil Jalagam
 
-active_calls=`sudo opensipsctl fifo dlg_list | grep dialog: | wc -l`
+active_calls=`sudo /usr/local/sbin/opensipsctl fifo dlg_list | grep dialog: | wc -l`
 echo "{\"active_calls\": $active_calls}"
